4ff2c0ef36c19ac9cc1571287e23b2a519e54194
Julia Parnis <juliaparnis@gmail.com>
2025-10-21 17:22:31 +0200

    Commit for the lab SQL select-2

--- solutions.sql
+++ solutions.sql
@@ -43,14 +43,13 @@ ORDER BY SUM(s.qty) DESC
 LIMIT 3;
 
 --Challenge 4 - Best Selling Authors Ranking
-SELECT a.au_id AS 'AUTHOR ID', 
-       a.au_lname AS 'LAST NAME', 
-       a.au_fname AS 'FIRST NAME', 
-	   SUM(s.qty) AS 'TOTAL'
+SELECT a.au_id AS 'AUTHOR ID',
+       a.au_lname AS 'LAST NAME',
+       a.au_fname AS 'FIRST NAME',
+       SUM(t.ytd_sales) AS 'TOTAL'
 FROM authors AS a
 JOIN titleauthor AS ta ON a.au_id = ta.au_id
 JOIN titles AS t ON ta.title_id = t.title_id
-JOIN sales AS s ON t.title_id = s.title_id
 GROUP BY a.au_id, a.au_lname, a.au_fname
-ORDER BY SUM(s.qty) DESC
+ORDER BY 'TOTAL' DESC
 LIMIT 23;

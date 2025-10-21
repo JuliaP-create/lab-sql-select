c59d0aeeab1a1ca840be836dd11a35282ac8a853
Julia Parnis <juliaparnis@gmail.com>
2025-10-21 17:26:29 +0200

    Commit for the lab SQL select-3

--- solutions.sql
+++ solutions.sql
@@ -46,10 +46,9 @@ LIMIT 3;
 SELECT a.au_id AS 'AUTHOR ID',
        a.au_lname AS 'LAST NAME',
        a.au_fname AS 'FIRST NAME',
-       SUM(t.ytd_sales) AS 'TOTAL'
+       COALESCE(SUM(t.ytd_sales), 0) AS 'TOTAL'
 FROM authors AS a
-JOIN titleauthor AS ta ON a.au_id = ta.au_id
-JOIN titles AS t ON ta.title_id = t.title_id
+LEFT JOIN titleauthor AS ta ON a.au_id = ta.au_id
+LEFT JOIN titles AS t ON ta.title_id = t.title_id
 GROUP BY a.au_id, a.au_lname, a.au_fname
-ORDER BY 'TOTAL' DESC
-LIMIT 23;
+ORDER BY 'TOTAL' DESC;
